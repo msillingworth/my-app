41c94556af0a77ab6e2cec0478121a6404895c55
Mark Illingworth <markillingworth@Marks-Mac-mini.lan>
2018-11-09 16:01:54 +0800

    Update today # 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div class="row">
+          <div id="menu" class="column">
+            <a href="http://www.google.com">Google</a>
+            <img src="assets/img/baseline_settings_black_24dp.png" alt=""></img>
+          </div>
+          <div id="content" class="column">Content</div>
+        </div> 
       </div>
     );
   }
